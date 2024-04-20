7032b581661d5e90a8a595d9fe78e6680943c213
Julia Nguyen <juliatnguyen05@gmail.com>
2024-04-20 14:59:03 -0700

    A container containing a card can be used many times to create contained cards, practical for including advice and information for the proposed study plan

---
+++ study/study/sorting_study.py
@@ -0,0 +1,46 @@
+"""
+Parsing information from the AI in order to
+format it in an interpretable way for Reflex
+"""
+
+# container for each section of the ai's feedback (divided by the time)
+class StudyPlan():
+    """Contains the data containing the study plan"""
+    def __init__(all_text: str):
+
+        def sort_text(all_text: str) -> list['Section']:
+            sorted_text = []
+            
+            pass 
+        
+        # a list consisting of strings seperated by their week (time) association
+        self._all_text = sort_text(all_text)
+
+    def get_all_text() -> list['Section']:
+        """Returns a list containing all the sorted tips from the
+        study plan, a string containing all the data"""
+        return self._all_text
+
+
+# an object consisting of the aspects of a particular week of advice 
+class Section():
+    def __init__(week: int, focus: str, activities: list[str]):
+        self._week = week
+        self._focus = focus
+        # the activities label can be hard coded into the ui since it won't be changed,
+        # only these activities
+        self._activities = activities
+
+    def get_week() -> int:
+        """Returns the week number"""
+        return self._week
+
+    def get_focus() -> str:
+        """Returns the focus that the user needs to prioritize"""
+        return self._focus
+
+    def get_activities() -> list[str]:
+        """returns a list of all the activities the user may partake in"""
+        return self._activities
+
+
--- study/study/study.py
+++ study/study/study.py
@@ -12,24 +12,60 @@ class State(rx.State):
     """The app state."""
     pass
 
+def contained_card(input_heading: str, input_text: str) -> rx.container:
+    """Makes a card and places it within a container, returns this 
+    container with a card in it"""
+    created_card = card(input_heading, input_text)
 
+    return container(created_card)
+
+
+def container(card: rx.card) -> rx.container:
+    """Creates a container to hold a card"""
+    return rx.container(
+        # card function... 
+        card
+    ),
+    size="3",
+
+
+def card(input_heading: str, input_text: str) -> rx.card:
+    """Creates a card that contains the information for a week
+    of a study plan"""
+    return rx.card(
+        rx.flex(
+            rx.box(
+                rx.heading(input_heading),
+                rx.text(
+                    input_text,
+                    width="100%"
+                ),
+            ),
+            spacing="1",
+        ),
+        as_child=True,
+        
+    )
+    
 def index() -> rx.Component:
+    # we're gonna test smth a little crazy...
     return rx.center(
         rx.theme_panel(),
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text("Get started by editing ", rx.code(filename)),
-            rx.button(
-                "Check out our docs!",
-                on_click=lambda: rx.redirect(docs_url),
-                size="4",
+            # we want a vertical stack of all the containers that will
+            # be created by parsing the text
+            rx.box(
+                rx.heading("Study Plan changed", size="8", padding='2'),
+                # contained_card('test1', 'apple'),
+                # contained_card('test2', 'no way....'),
+                # card('test1', 'apple'),
+                # card('test2', 'no way....'),
+               
+                # rx.text("Get started by editing ", rx.code(filename)),
             ),
-            rx.logo(),
-            align="center",
-            spacing="7",
-            font_size="2em",
+            spacing='2',
         ),
-        height="100vh",
+        height="50vh",
     )
 
 
