30ce2373d2b94434f171de5dade2f8ad71f6a94e
sairiinxw <156254463+sairiinxw@users.noreply.github.com>
2024-04-20 13:43:44 -0700

    Safe version 3 with header and fixed welcome

--- studydinohome/studydinohome/studydinohome.py
+++ studydinohome/studydinohome/studydinohome.py
@@ -4,21 +4,54 @@ import reflex as rx
 
 docs_url = "https://reflex.dev/docs/getting-started/introduction/"
 filename = f"{config.app_name}/{config.app_name}.py"
+pg_size = "150"
 
 class State(rx.State):
     """The app state."""
 
-header1_style = {
+header1_style = { #sets font for header
     "font_family": "Arial Rounded MT Bold",
 }
+def navigation():
+    return rx.box(
+        rx.hstack(
+            rx.link(
+                rx.hstack(
+                    rx.heading("studydino | merging AI and mental health", size = "6",
+                               style=header1_style),
+                    align="center",
+                ),
+                href="/",
+            ),
+            rx.spacer(width="100%"),
+            #menu here
+            align="center",
+            justify="center",
+            border_bottom="0.2em solid #F0F0F0",
+            padding_x="10em",
+            padding="2em",
+            bg="rgba(255, 255, 255, 0.97)",
+        ),
+        position="fixed",
+        width="100%",
+        topy="0px",
+        z_index="500",
+        height=pg_size + "%",
+    )
+
 
 def index() -> rx.Component:
     return rx.center(
+        navigation(),
         rx.vstack(
-            rx.heading("Welcome to studydino", size="9", style=header1_style),
-            rx.text("where academics meet AI and mental health",
-                size="7",
-                font_family="Arial Rounded MT Bold",
+            rx.vstack(
+                rx.heading("Welcome to studydino", size="9", style=header1_style),
+                rx.text("where academics meet AI and mental health",
+                    size="6",
+                    font_family="Arial Rounded MT Bold",
+                ),
+                align="center",
+                spacing="2",
             ),
             rx.button(
                 "Form my custom study plan",
@@ -26,15 +59,20 @@ def index() -> rx.Component:
                 size="4",
             ),
             align="center",
-            spacing="7",
+            spacing="9",
             font_size="2em",
+            height="70vh",
         ),
-        height="100vh",
-        background="radial-gradient(circle at 22% 11%,rgba(229, 255, 231,0.8),hsla(0,0%,100%,0) 30%),radial-gradient(circle at 82% 40%,rgba(253, 251, 230,0.8),hsla(0,0%,100%,0) 45%),radial-gradient(circle at 25% 85%,rgba(255, 234, 242, 0.8),hsla(0,0%,100%,0) 40%)",
+        padding="1em",
+        height=pg_size + "vh",
+        background="radial-gradient(circle at 22% 11%,rgba(229, 255, 231,0.8),hsla(0,0%,100%,0) 30%),radial-gradient(circle at 82% 40%,rgba(253, 251, 230,0.8),hsla(0,0%,100%,0) 45%),radial-gradient(circle at 15% 85%,rgba(255, 227, 237, 0.8),hsla(0,0%,100%,0) 40%)",
     )
 
 style1 = {
     "font_family": "Arial",
+    rx.heading: {
+        "color": "black",
+    },
     rx.button: {
         "background_color": "#C3E3C3", #default CSS color: DarkSeaGreen
         "color": "black",
