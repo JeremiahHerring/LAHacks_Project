c050ce5a2f6e582ec5fe2dde010148d4fa0c0a08
Julia Nguyen <juliatnguyen05@gmail.com>
2024-04-20 06:45:15 -0700

    Centered box for uploading files

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ThatN\OneDrive\Desktop\reflex\.venv
+set VIRTUAL_ENV=C:\Users\Julia\LAHacks_Project\LAHacks_Project\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
