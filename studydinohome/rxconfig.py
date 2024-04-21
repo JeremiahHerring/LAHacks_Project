198361228d125b46072cf37d94fb6790b0ee4466
sairiinxw <156254463+sairiinxw@users.noreply.github.com>
2024-04-21 02:44:22 -0700

    Safe version Final_1 published at studydino.reflex.run with light/dark mode, dialog, UI for upload

--- studydinohome/rxconfig.py
+++ studydinohome/rxconfig.py
@@ -2,4 +2,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="studydinohome",
+    # api_url="http://studydino.com:8000",
 )
--- upload/upload/upload.py
+++ upload/upload/upload.py
@@ -17,6 +17,21 @@ import google.generativeai as genai
 docs_url = "https://reflex.dev/docs/getting-started/introduction/"
 filename = f"{config.app_name}/{config.app_name}.py"
 color = "rgb(107,99,246)"
+pg_size = "150"
+black = "rgba(0, 0, 0, 0.97)"
+white = "rgba(255, 255, 255, 0.97)"
+
+header1_style = { #sets font for header
+    "font_family": "Arial Rounded MT Bold",
+}
+
+style1 = {
+    "font_family": "Arial",
+    rx.button: {
+        "background_color": "#C3E3C3", #default CSS color: DarkSeaGreen
+        "color": "black",
+    },
+}
 
 def upload_to_gemini(pdf_path):
     """Upload the PDF file to GeminiAPI."""
@@ -55,44 +70,97 @@ class State(rx.State):
             except Exception as e:
                 print(f"Error handling upload: {e}")
 
+def navigation():
+    return rx.box(
+        rx.hstack(
+            rx.link(
+                rx.hstack(
+                    rx.image(src="/dino_trans.png", width="50px"),
+                    rx.heading(rx.text.em("studydino"), " | merging AI and mental health", size = "5",
+                               style=header1_style, color=rx.color_mode_cond(black, white)
+                    ),
+                    align="center",
+                ),
+                href="/",
+            ),
+            rx.spacer(width="100%"),
+            #menu here
+            rx.color_mode.button(rx.color_mode.icon(), size="3", float="right", color=rx.color_mode_cond(black, white), background_color=rx.color_mode_cond("#C3E3C3", "#568356")),
+            bg=rx.color_mode_cond(white, black),
+            align="center",
+            justify="center",
+            border_bottom="0.2em solid #F0F0F0",
+            padding_x="10em",
+            padding="1.5em",
+        ),
+        position="fixed",
+        width="100%",
+        top="0px",
+        z_index="500",
+        height="50px",
+    )
+
 def index():
     """The main view"""
     return rx.center(
+        navigation(),
         rx.vstack(
-                rx.upload(
-                    rx.vstack(
-                        rx.button("Select File", color=color, bg="white", border=f"1px solid {color}",),
-                        rx.text("Drag and drop files here or click to select files"),
+            rx.heading("Your upload page", size="8", style=header1_style),
+            rx.upload(
+                rx.vstack(
+                    rx.button("Select file", bg="white", size="4", radius="full", color=rx.color_mode_cond(black, white), background_color=rx.color_mode_cond("#C3E3C3", "#568356")),
+                    rx.text(
+                        "Drag and drop files here or click to select files",
+                        size="7",
+                        font_family="Arial",
                     ),
-                    id="upload_file",
-                    border=f"1px dotted {color}",
-                    padding="5em",
-                    align="center",
-                    
+                    spacing="7",
                 ),
-                rx.hstack(rx.foreach(rx.selected_files("upload_file"), rx.text)),
-                rx.flex(
-                    rx.button(
-                        "Upload",
-                        on_click=State.handle_upload(rx.upload_files(upload_id="upload_file")),
-                    ),
-                    rx.button(
-                        "Clear",
-                        on_click=rx.clear_selected_files("upload_file"),
-                    ),
-                    
-                    rx.foreach(State.img, lambda img: rx.image(src=rx.get_upload_url(img))),
-
-                    spacing="2",
-                    align="center",
-
+                id="upload_file",
+                border=f"1px dotted {rx.color_mode_cond(black, white)}",
+                border_radius="25px",
+                background_color=rx.color_mode_cond(white, black),
+                padding="5em",
+                align="center",
+                width="85%",    
+            ),
+            rx.hstack(rx.foreach(rx.selected_files("upload_file"), rx.text)),
+            rx.flex(
+                rx.button(
+                    "Upload",
+                    on_click=State.handle_upload(rx.upload_files(upload_id="upload_file")),
+                    size="4",
+                    radius="full",
+                    background_color=rx.color_mode_cond("#C3E3C3", "#568356"),
+                    color=rx.color_mode_cond(black, white),
+                ),
+                rx.button(
+                    "Clear",
+                    on_click=rx.clear_selected_files("upload_file"),
+                    size="4",
+                    radius="full",
+                    background_color=rx.color_mode_cond("#C3E3C3", "#568356"),
+                    color=rx.color_mode_cond(black, white),
                 ),
                 
-            
-                padding="5em",
-        )
+                rx.foreach(State.img, lambda img: rx.image(src=rx.get_upload_url(img))),
 
+                spacing="3",
+                align="center",
+            ),
+            align="center",
+            spacing="5",
+            font_size="2em",
+            height="110vh",
+        ),
+        padding="1em",
+        height=pg_size + "vh",
+        background="radial-gradient(circle at 22% 11%,rgba(229, 255, 231,0.8),hsla(0,0%,100%,0) 30%),radial-gradient(circle at 82% 40%,rgba(253, 251, 230,0.8),hsla(0,0%,100%,0) 45%),radial-gradient(circle at 15% 85%,rgba(255, 227, 237, 0.8),hsla(0,0%,100%,0) 40%)",
     )
 
-app = rx.App()
-app.add_page(index)
+app = rx.App(style=style1,
+             theme=rx.theme(
+                appearance="light", has_background=True, radius="full"
+            )
+            )
+app.add_page(index, title="studydino | upload")
