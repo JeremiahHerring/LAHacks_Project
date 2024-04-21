04a57feed51e3bdcebc0dfc0d6c7a661d27b65de
Julia Nguyen <juliatnguyen05@gmail.com>
2024-04-20 22:40:27 -0700

    centered heading, fixed padding

--- studyplan/studyplan/studyplan.py
+++ studyplan/studyplan/studyplan.py
@@ -9,8 +9,8 @@ filename = f"{config.app_name}/{config.app_name}.py"
 class State(rx.State):
     """The app state."""
     # include the given text
-    ai_plan: str = '**Study Plan:**\n\n**Week 1:**\n\n* **Focus:** Introduction to Syntax Analysis, Grammar, and Chomsky Hierarchy. \n\n* **Activities:**\n\n* Read sections 3.1-3.3 of the textbook.\n\n* Create flashcards for key terms and concepts.\n\n* Practice writing simple grammars in BNF notation.\
-        \n\n* **Mindfulness:** Start each study session with a 5-minute breathing exercise.\n\n**Week 2:**\n\n* **Focus:** Left Recursion, Backtracking, and Top-Down Parsing (RDP).\n\n* **Activities:**\n\n* Read sections 3.4-3.5a of the textbook.'.replace('*', '')
+    ai_plan: str = 'ai_input ai_input ai_input \n\nai_input ai_input ai_input ai_input \n\nai_input ai_input \n\nai_input ai_input ai_input ai_input ai_input ai_input ai_input ai_input ai_input'.replace('*', '').replace('\n', '\n\n')
+
 
 def index() -> rx.Component:
     # we're gonna test smth a little crazy...
@@ -18,7 +18,7 @@ def index() -> rx.Component:
         rx.vstack(
             rx.box(
                 rx.container(
-                    rx.heading("Study Plan for [we'll figure it out]", size="8", padding='70px 25px', align="center"),
+                    rx.heading("Study Plan for ", rx.text.em("placeholder"), size="8", padding='25px 25px', align='center'),
                     rx.container(
                         rx.markdown(State.ai_plan),
                         padding='25px 25px'
